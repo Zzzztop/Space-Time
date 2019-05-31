21e114d4a18d945f75208a39a667c0f0523d80d7
monkey <13838121279@163.com>
2019-05-31 17:48:48 +0800

    在实体类中使用lombok注解

--- spring-portal/admin-login/src/main/java/com/ztop/admin/bean/UserInfo.java
+++ spring-portal/admin-login/src/main/java/com/ztop/admin/bean/UserInfo.java
@@ -1,10 +1,15 @@
 package com.ztop.admin.bean;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @Author ZhouJianGuo
  * @Date 2019/5/22 16:54
  * @Version 1.0
  **/
+@Data
+@ToString
 public class UserInfo {
     private String id;
     private String token;
@@ -18,108 +23,4 @@ public class UserInfo {
     private String permission;
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getInco() {
-        return inco;
-    }
-
-    public void setInco(String inco) {
-        this.inco = inco;
-    }
-
-    public String getCreate_time() {
-        return create_time;
-    }
-
-    public void setCreate_time(String create_time) {
-        this.create_time = create_time;
-    }
-
-    public String getUpdata_time() {
-        return updata_time;
-    }
-
-    public void setUpdata_time(String updata_time) {
-        this.updata_time = updata_time;
-    }
-
-    public String getOutdata_time() {
-        return outdata_time;
-    }
-
-    public void setOutdata_time(String outdata_time) {
-        this.outdata_time = outdata_time;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPermission() {
-        return permission;
-    }
-
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    @Override
-    public String toString() {
-        return "UserInfo{" +
-                "id='" + id + '\'' +
-                ", token='" + token + '\'' +
-                ", name='" + name + '\'' +
-                ", inco='" + inco + '\'' +
-                ", create_time='" + create_time + '\'' +
-                ", updata_time='" + updata_time + '\'' +
-                ", outdata_time='" + outdata_time + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", permission='" + permission + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
--- spring-portal/admin-login/src/main/java/com/ztop/admin/bean/vo/LoginRespVo.java
+++ spring-portal/admin-login/src/main/java/com/ztop/admin/bean/vo/LoginRespVo.java
@@ -1,10 +1,15 @@
 package com.ztop.admin.bean.vo;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @Author ZhouJianGuo
  * @Date 2019/5/22 17:02
  * @Version 1.0
  **/
+@Data
+@ToString
 public class LoginRespVo {
     private String id;
     private String token;
